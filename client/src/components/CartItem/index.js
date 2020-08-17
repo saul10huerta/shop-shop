9ac981a159b3510fcf7242f694f825493c3a31d4
Saul Everardo Huerta Perez <Saul@Sauls-MBP.lan>
2020-08-16 21:56:52 -0500

    Implemented Shopping Cart Storage

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { idbPromise } from "../../utils/helpers";
 
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
@@ -8,26 +9,31 @@ const CartItem = ({ item }) => {
     const [, dispatch] = useStoreContext();
 
     const removeFromCart = item => {
-    dispatch({
-        type: REMOVE_FROM_CART,
-        _id: item._id
-    });
+        dispatch({
+          type: REMOVE_FROM_CART,
+          _id: item._id
+        });
+        idbPromise('cart', 'delete', { ...item });
     };
 
     const onChange = (e) => {
         const value = e.target.value;
       
         if (value === '0') {
-          dispatch({
-            type: REMOVE_FROM_CART,
-            _id: item._id
-          });
+            dispatch({
+              type: REMOVE_FROM_CART,
+              _id: item._id
+            });
+          
+            idbPromise('cart', 'delete', { ...item });
         } else {
-          dispatch({
-            type: UPDATE_CART_QUANTITY,
-            _id: item._id,
-            purchaseQuantity: parseInt(value)
-          });
+            dispatch({
+              type: UPDATE_CART_QUANTITY,
+              _id: item._id,
+              purchaseQuantity: parseInt(value)
+            });
+          
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
         }
     };
 
